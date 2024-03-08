85d56389e1e02bbe5797f1cbc6956282276803bc
v_maolinye <v_maolinye@tencent.com>
2024-03-08 18:01:11 +0800

    随机生成可销毁方块

--- Source/Bomberman/BlockGenerator.cpp
+++ Source/Bomberman/BlockGenerator.cpp
@@ -8,10 +8,16 @@ ABlockGenerator::ABlockGenerator()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	static ConstructorHelpers::FClassFinder<ABlock> UB(TEXT("/Game/Blueprint/BP_Block.uasset"));
-	Block = UB.Class;
-	static ConstructorHelpers::FClassFinder<ABreakableBlock>BB(TEXT("/Game/Blueprint/BP_BreakableBlock.asset"));
-	BreakableBlock = BB.Class;
+	//static ConstructorHelpers::FClassFinder<ABlock> UB(TEXT("/Game/Blueprint/BP_Block"));
+	//Block = UB.Class;
+	//static ConstructorHelpers::FClassFinder<ABreakableBlock>BB(TEXT("/Game/Blueprint/BP_BreakableBlock"));
+	//BreakableBlock = BB.Class;
+	SpawnPlayerPositions = {
+	FIntPoint(0, 0), FIntPoint(0, 1), FIntPoint(1, 0),
+	FIntPoint(Width - 1, 0), FIntPoint(Width - 1, 1), FIntPoint(Width - 2, 0),
+	FIntPoint(0, Height - 1), FIntPoint(0, Height - 2), FIntPoint(1, Height - 1),
+	FIntPoint(Width - 1, Height - 1), FIntPoint(Width - 1, Height - 2), FIntPoint(Width - 2, Height - 1)
+	};
 }
 
 // Called when the game starts or when spawned
@@ -19,6 +25,8 @@ void ABlockGenerator::BeginPlay()
 {
 	Super::BeginPlay();
 	SpawnBlock(); // 必须在对象完全初始化之后调用，否则GetWorld返回nullptr
+	FindSpawnBBPosition();
+	SpawnBreakBlock();
 }
 
 // Called every frame
@@ -37,5 +45,30 @@ void ABlockGenerator::SpawnBlock()
 				GetWorld()->SpawnActor<ABlock>(Block, Position, FRotator::ZeroRotator);
 			}
 		}
+	}
+}
+
+void ABlockGenerator::FindSpawnBBPosition()
+{
+	for (int i = 0; i < Width; i++) {
+		for (int j = 0; j < Height; j++) {
+			if (SpawnPlayerPositions.Contains(FIntPoint(i, j))) {
+				continue;
+			}
+			if (i % 2 == 0 || j % 2 == 0) {
+				FVector Position = FVector((i - Width / 2) * BlockSize + 50, (j - Height / 2) * BlockSize + 50, 150);
+				BreakBlockPositions.Add(Position);
+			}
+		}
+	}
+}
+
+void ABlockGenerator::SpawnBreakBlock()
+{
+	int BreakBlockNum = BreakBlockPositions.Num() * BlockDensity;
+	for (int i = 0; i < BreakBlockNum; i++) {
+		int Index = FMath::RandRange(0, BreakBlockPositions.Num() - 1);
+		GetWorld()->SpawnActor<ABreakableBlock>(BreakableBlock, BreakBlockPositions[Index], FRotator::ZeroRotator);
+		BreakBlockPositions.RemoveAt(Index);
 	}
 }
--- Source/Bomberman/BlockGenerator.h
+++ Source/Bomberman/BlockGenerator.h
@@ -31,7 +31,15 @@ protected:
 	int Width = 15;
 	UPROPERTY(EditAnywhere, Category = "Block Generation|Setting")
 	int BlockSize = 100;
+	UPROPERTY(VisibleAnywhere, Category = "Block Generation|Setting")
+	TArray<FVector>BreakBlockPositions; // 可以生成可销毁方块的位置
+	UPROPERTY(EditAnywhere, Category = "Block Generation|Setting")
+	TArray<FIntPoint>SpawnPlayerPositions; // 玩家出生的位置，不可以有方块
+	UPROPERTY(EditAnywhere, meta = (ClampMin = "0", ClampMax = "1"), Category = "Block Generation|Setting")
+	float BlockDensity=1;
 	void SpawnBlock();
+	void FindSpawnBBPosition();
+	void SpawnBreakBlock();
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
